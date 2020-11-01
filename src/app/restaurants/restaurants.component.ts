2e4f79e054b767f0b72de27e30be1b4d6e24438e
Mystrader <mystrader@gmail.com>
2020-11-01 00:36:05 -0300

    46 -  Criando Componentes de Detalhe de Restaurante

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant/restaurant.model';
+import { RestaurantService } from './restaurants.service';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+  
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[]
+
+  constructor(private RestaurantService: RestaurantService) { }
+
+  ngOnInit() {
+    this.RestaurantService.restaurants()
+    .subscribe( restaurants => this.restaurants = restaurants)
+  }
+
+}
