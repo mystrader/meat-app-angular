ba0a9f6a0225b123531f1e46fa27f2ce6079ac57
Mystrader <mystrader@gmail.com>
2020-11-01 16:32:22 -0300

    50. Implementando o Componente de Avaliações - Pipes Async e Date

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+import { RestaurantService } from '../restaurants/restaurants.service';
+import { Restaurant } from '../restaurants/restaurant/restaurant.model';
+
 
 @Component({
   selector: 'mt-restaurant-detail',
   templateUrl: './restaurant-detail.component.html'
 })
-export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+
+export class RestaurantDetailComponent implements OnInit {
+ 
+  restaurant : Restaurant
+
+  constructor(private restaurantsService : RestaurantService,
+              private route : ActivatedRoute ) { }
 
   ngOnInit() {
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+    .subscribe(restaurant => this.restaurant = restaurant)
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -20,6 +20,20 @@ export class RestaurantService{
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
+
+    restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(reponse => reponse.json()) //no angular é um método
+        .catch(ErrorHandler.handleError)
+    }
+
+    //Este aqui é para rota filha
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(reponse => reponse.json()) //no angular é um método
+        .catch(ErrorHandler.handleError)
+    }
+
 }
 
 
