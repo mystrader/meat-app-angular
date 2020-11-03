baab194ac4f7d9fea4a139a8e5f9739b04b17081
Mystrader <mystrader@gmail.com>
2020-11-02 21:39:50 -0300

    53 - carrinho de compras

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -9,14 +9,24 @@ export class ShoppingCartComponent implements OnInit {
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
-  ngOnInit() {    
+  ngOnInit() {
 
   }
   items(): any[]{
     return this.shoppingCartService.items;
   }
+
+  clear(){
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item: any){
+    this.shoppingCartService.removeItem(item)
+  }
+  addItem(item: any){
+    this.shoppingCartService.addItem(item)
+  }
   total(): number{
     return this.shoppingCartService.total()
   }
-
 }
